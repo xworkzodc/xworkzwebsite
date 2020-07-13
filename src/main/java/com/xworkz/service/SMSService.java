68cd2af123622c23034fe67b8e229dfbfbf0f484
vinay-coder1 <vinays10998@gmail.com>
2020-07-13 19:43:39 +0530

    updated by reviews3

--- src/main/java/com/xworkz/service/SMSService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.service;
-
-public interface SMSService {
-  
-	public String sendCampaign(String apiKey, String secretKey, String useType, String phone, String message,
-			String senderId);
-	
-	public String reports(String usetype, String apiKey, String secretKey, String fromDate, String toDate);
-}
--- src/main/java/com/xworkz/service/SpringMailService.java
+++
@@ -1,6 +0,0 @@
-package com.xworkz.service;
-
-public interface SpringMailService {
-
-	public boolean validateAndSendMailByMailId(String to ,String subject ,String body);
-}
