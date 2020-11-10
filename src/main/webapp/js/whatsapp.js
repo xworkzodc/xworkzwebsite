9f43bdfe3b6f9a08519612edd51cfca30785bc57
VinayTheCoder <56579349+vinay-coder1@users.noreply.github.com>
2020-11-10 19:16:33 +0530

    updated with Interview Questions module

---
+++ src/main/webapp/js/whatsapp.js
@@ -0,0 +1,13 @@
+$(function () {
+           $('#WAButton').floatingWhatsApp({
+               phone: '919886971483',
+               headerTitle: 'Chat with us on WhatsApp!', 
+               popupMessage: 'Hello, how can we help you?', 
+               showPopup: true, 
+               buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+               headerColor: 'black',
+               backgroundColor: 'black', 
+               position: "left" 
+
+           });
+       });
